82fb2bdbc6ec8ed87776d23dacf2b25c92b18e46
Roger Wernersson <roger.wernersson@sigmatechnology.se>
2024-05-23 19:16:46 +0200

    session four

---
+++ src/main/java/se/sigmatechnology/codingdojo/ddd02/orgchart/Orgianization.java
@@ -0,0 +1,18 @@
+package se.sigmatechnology.codingdojo.ddd02.orgchart;
+
+public class Orgianization {
+
+    void setup() {
+
+//        con = Rest.of("somewhere");
+//        con = TLS.to(con);
+//        con = Reconnect.to(con);
+//
+//        LENGTH = Unit("length")
+//        meter = UnitOfMeasurement("meter", LENGTH);
+//        centimeter = UnitOfMeasurement("cm", LENGTH);
+//        LENGTH.baseUnit(meter);
+//        LENGTH.from(meter).to(centimeter).by(100);
+//        Measurement m(UnitOfMeasurement, Value)
+    }
+}
